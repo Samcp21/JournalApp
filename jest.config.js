f5bd0b5f2efa3449c9033e79d66d2c8093f5c1db
Samir Checcllo <samcp210@gmail.com>
2024-07-03 21:32:53 -0500

    Revert "fix"
    This reverts commit 78062ae7d93dff6f3cd3c6b349239b2d74e682d6.

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "jest-environment-jsdom",
-  setupFiles: ["./jest.setup.js"],
-  transform: {
-    "^.+\\.jsx?$": ["babel-jest", { configFile: "./babel.config.cjs" }],
-  },
-};
